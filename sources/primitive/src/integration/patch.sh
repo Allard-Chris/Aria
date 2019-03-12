cd57f4281db6effda01c5a92eadbfd91e1ccb340
Corentin Aulagnier <corentin.aulagnier@orange.com>
2019-03-12 19:51:03 +0100

    modifying patch script

--- sources/primitive/src/integration/patch.sh
+++ sources/primitive/src/integration/patch.sh
@@ -1,17 +1,34 @@
 #! /bin/sh
 
+# Path to the libressl repo
+path = "../../../libressl-2.9.0"
 
-cd libressl-2.9.0
 
-# Creating file for aria primitive
-mkdir crypto/aria
+##### Adding files for aria primitive
+# Creating folder for aria primitive
+mkdir $(path)/crypto/aria
 
+# Copy all aria files on openssl
+cp ../aria.c 	$(path)/crypto/aria/
+cp ../aria_core.c $(path)/crypto/aria/
+cp ../aria_utils.c $(path)/crypto/aria/
+
+cp ../aria_const.h $(path)/crypto/aria/
+cp ../aria_core.h $(path)/crypto/aria/
+cp ../aria_debug.h $(path)/crypto/aria/
+cp ../aria_test.h $(path)/crypto/aria/
+cp ../aria_type.h $(path)/crypto/aria/
+cp ../aria_utils.h $(path)/crypto/aria/
+
+# Modifiyng makefile for aria primitive
+cp Makefile.am.crypto $(path)/crypto/Makefile.am
+
+
+##### Adding aria.h for openssl using
 # Copy aria.h on openssl
-cp .../aria.c crypto/aria/aria.c
+cp aria.h $(path)/include/openssl/aria.h
 
-# Copy aria.h on openssl
-cp .../aria.h include/openssl/aria.h
+# Modifiyng makefile for aria.h
+cp Makefile.am.include.openssl $(path)/include/openssl/Makefile.am
 
-# Copy aria.h on openssl
-cp .../build.info .../build.info
 
