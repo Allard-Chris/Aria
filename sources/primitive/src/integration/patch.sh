285f100c7313161e76071e672d7a5ac87c5a2f17
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2019-03-20 18:46:56 +0100

    major modifications for integration before testing

--- sources/primitive/src/integration/patch.sh
+++ sources/primitive/src/integration/patch.sh
@@ -1,34 +1,40 @@
 #! /bin/sh
 
-# Path to the libressl repo
-path="../../../libressl-2.9.0"
+# Important Path for patching
+libressl_path="../../../libressl-2.9.0"
 
 
-##### Adding files for aria primitive
+#################################################
+##### libressl/app/openssl
+cp ./app/openssl/openssl.c $libressl_path/app/openssl/openssl.c
+cp ./app/openssl/genrsa.c $libressl_path/app/openssl/genrsa.c
+cp ./app/openssl/gendsa.c $libressl_path/app/openssl/gendsa.c
+cp ./app/openssl/smime.c $libressl_path/app/openssl/smime.c
+cp ./app/openssl/pkcs12.c $libressl_path/app/openssl/pkcs12.c
+cp ./app/openssl/speed.c $libressl_path/app/openssl/speed.c
+
+
+#################################################
+##### libressl/crypto
 # Creating folder for aria primitive
-mkdir $path/crypto/aria
+mkdir $libressl_path/crypto/aria
 
-# Copy all aria files on openssl
-cp ../aria_core.c $path/crypto/aria/aria_core.c
-cp ../aria_utils.c $path/crypto/aria/aria_utils.c
+# Copy all aria files on crypto/aria
+cp ./crypto/aria/aria_core.c $libressl_path/crypto/aria/aria_core.c
+cp ./crypto/aria/aria_locl.h $libressl_path/crypto/aria/aria_locl.h
 
-cp ../aria_const.h $path/crypto/aria/aria_const.h
-cp ../aria_core.h $path/crypto/aria/aria_core.h
-cp ../aria_debug.h $path/crypto/aria/aria_debug.h
-cp ../aria_test.h $path/crypto/aria/aria_test.h
-cp ../aria_type.h $path/crypto/aria/aria_type.h
-cp ../aria_utils.h $path/crypto/aria/aria_utils.h
-cp aria_locl.h $path/crypto/aria/aria_locl.h
-
-# Modifiyng makefile for aria primitive
-cp Makefile.am.crypto $path/crypto/Makefile.am
+# Modifiyng makefiles and other files
+cp ./crypto/Makefile.am $libressl_path/crypto/Makefile.am
+cp ./crypto/objects/obj_dat.h $libressl_path/crypto/objects/obj_dat.h # no modifications for now
+cp ./crypto/CMakefile.txt $libressl_path/crypto/CMakefile.txt
+cp ./crypto/Makefile.in $libressl_path/crypto/Makefile.in
 
 
-##### Adding aria.h for openssl using
-# Copy aria.h on openssl
-cp aria.h $path/include/openssl/aria.h
+#################################################
+##### libressl/include/openssl
+cp ./include/openssl/aria.h $libressl_path/include/openssl/aria.h
 
-# Modifiyng makefile for aria.h
-cp Makefile.am.include.openssl $path/include/openssl/Makefile.am
-
-
+# Modifiyng makefiles and other files
+cp ./include/openssl/Makefile.am $libressl_path/include/openssl/Makefile.am
+cp ./include/openssl/obj_mac.h $libressl_path/include/openssl/obj_mac.h
+cp ./include/openssl/Makefile.in $libressl_path/include/openssl/Makefile.in
