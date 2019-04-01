8c6beaeb5d42369cad0a37b6b036943dfb781fb0
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2019-04-01 16:43:28 +0200

    modifiyng script path inside patch script

--- sources/primitive/src/integration/patch.sh
+++ sources/primitive/src/integration/patch.sh
@@ -2,16 +2,16 @@
 
 # Important Path for patching
 libressl_path="../../../libressl-2.9.0"
-
+script_path="$( cd "$(dirname "$0")" ; pwd -P )"
 
 #################################################
 ##### libressl/app/openssl
-cp ./apps/openssl/openssl.c $libressl_path/apps/openssl/openssl.c
-cp ./apps/openssl/genrsa.c $libressl_path/apps/openssl/genrsa.c
-cp ./apps/openssl/gendsa.c $libressl_path/apps/openssl/gendsa.c
-cp ./apps/openssl/smime.c $libressl_path/apps/openssl/smime.c
-cp ./apps/openssl/pkcs12.c $libressl_path/apps/openssl/pkcs12.c
-cp ./apps/openssl/speed.c $libressl_path/apps/openssl/speed.c
+cp $script_path/apps/openssl/openssl.c $libressl_path/apps/openssl/openssl.c
+cp $script_path/apps/openssl/genrsa.c $libressl_path/apps/openssl/genrsa.c
+cp $script_path/apps/openssl/gendsa.c $libressl_path/apps/openssl/gendsa.c
+cp $script_path/apps/openssl/smime.c $libressl_path/apps/openssl/smime.c
+cp $script_path/apps/openssl/pkcs12.c $libressl_path/apps/openssl/pkcs12.c
+cp $script_path/apps/openssl/speed.c $libressl_path/apps/openssl/speed.c
 
 
 #################################################
@@ -20,21 +20,21 @@ cp ./apps/openssl/speed.c $libressl_path/apps/openssl/speed.c
 mkdir $libressl_path/crypto/aria
 
 # Copy all aria files on crypto/aria
-cp ./crypto/aria/aria_core.c $libressl_path/crypto/aria/aria_core.c
-cp ./crypto/aria/aria_locl.h $libressl_path/crypto/aria/aria_locl.h
+cp $script_path/crypto/aria/aria_core.c $libressl_path/crypto/aria/aria_core.c
+cp $script_path/crypto/aria/aria_locl.h $libressl_path/crypto/aria/aria_locl.h
 
 # Modifiyng makefiles and other files
-cp ./crypto/Makefile.am $libressl_path/crypto/Makefile.am
-cp ./crypto/objects/obj_dat.h $libressl_path/crypto/objects/obj_dat.h # no modifications for now
-cp ./crypto/CMakeLists.txt $libressl_path/crypto/CMakefile.txt
-cp ./crypto/Makefile.in $libressl_path/crypto/Makefile.in
-cp ./crypto/Makefile $libressl_path/crypto/Makefile
+cp $script_path/crypto/Makefile.am $libressl_path/crypto/Makefile.am
+cp $script_path/crypto/objects/obj_dat.h $libressl_path/crypto/objects/obj_dat.h # no modifications for now
+cp $script_path/crypto/CMakeLists.txt $libressl_path/crypto/CMakefile.txt
+cp $script_path/crypto/Makefile.in $libressl_path/crypto/Makefile.in
+cp $script_path/crypto/Makefile $libressl_path/crypto/Makefile
 
 #################################################
 ##### libressl/include/openssl
-cp ./include/openssl/aria.h $libressl_path/include/openssl/aria.h
+cp $script_path/include/openssl/aria.h $libressl_path/include/openssl/aria.h
 
 # Modifiyng makefiles and other files
-cp ./include/openssl/Makefile.am $libressl_path/include/openssl/Makefile.am
-cp ./include/openssl/obj_mac.h $libressl_path/include/openssl/obj_mac.h  # no modifications for now
-cp ./include/openssl/Makefile.in $libressl_path/include/openssl/Makefile.in
+cp $script_path/include/openssl/Makefile.am $libressl_path/include/openssl/Makefile.am
+cp $script_path/include/openssl/obj_mac.h $libressl_path/include/openssl/obj_mac.h  # no modifications for now
+cp $script_path/include/openssl/Makefile.in $libressl_path/include/openssl/Makefile.in
