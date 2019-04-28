954d8a22f3f5e8d2dc38968bd099dd471cb0c37d
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2019-04-28 14:50:48 +0200

    [Repo Ready] mooving aria.patch inside libressl

--- sources/patch.sh
+++ sources/patch.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 # This bash will create the patch file for libressl-aria inside libressl
 
-diff -Naur ./libressl-2.9.0 ./libressl-2.9.0-aria > aria.patch
+diff -Naur libressl-2.9.0 libressl-2.9.0-aria > libressl-2.9.0/aria.patch
+
+# To apply the patch : cd libressl-2.9.0 && patch aria.patch
