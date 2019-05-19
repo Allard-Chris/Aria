57a6d29d49024c40f1e81eaa4a45fb9ff213907a
Allard <lyon.allard.chris@gmail.com>
2019-05-19 18:29:22 +0200

    Cleaning branch

--- sources/patch.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# This bash will create the patch file for libressl-aria inside libressl
-
-diff -Naur libressl-2.9.0 libressl-2.9.0-aria > libressl-2.9.0/aria.patch
-
-# To apply the patch : cd libressl-2.9.0 && patch aria.patch
