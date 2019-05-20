e1ea04d788d2b787e2fb993d4797b5be8648aa71
Allard <lyon.allard.chris@gmail.com>
2019-05-20 22:56:20 +0200

    Change aria.h

--- sources/libressl-2.9.0a/crypto/evp/e_aria.c
+++ sources/libressl-2.9.0a/crypto/evp/e_aria.c
@@ -1,5 +1,9 @@
 /* ARIA subkey Structure */
+#include <string.h>
+
 #include <openssl/opensslconf.h>
+
+#ifndef OPENSSL_NO_ARIA
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/aria.h>
@@ -12,31 +16,23 @@ typedef struct {
     ARIA_KEY ks;
 } EVP_ARIA_KEY;
 
-IMPLEMENT_BLOCK_CIPHER(
-    aria_128, /* cname */
-    ks, /* ksched */
-    Aria, /* cprefix */
-    EVP_ARIA_KEY, /* kstruct */
-    NID_aria_128, /* NID */
-    16, /* block_size */
-    16, /* key_len */
-    16, /* iv_len */
-    128, /* cbits */
-    0, /* flags */
-    aria_init_key, /* init_key */
-    NULL, /* cleanup */
-    EVP_CIPHER_set_asn1_iv, /* set_asn1 */
-    EVP_CIPHER_get_asn1_iv, /* get_asn1 */
-    NULL /* ctrl */
-)
+/* Attribute operation for Aria */
+#define data(ctx)	EVP_C_DATA(EVP_ARIA_KEY,ctx)
+
+IMPLEMENT_BLOCK_CIPHER(aria_128, ks, Aria, EVP_ARIA_KEY,
+    NID_aria_128, 16, 16, 16, 128,
+    0, aria_init_key, NULL,
+    EVP_CIPHER_set_asn1_iv,
+    EVP_CIPHER_get_asn1_iv,
+    NULL)
 IMPLEMENT_BLOCK_CIPHER(aria_192, ks, Aria, EVP_ARIA_KEY,
-    NID_aria_128, 16, 24, 16, 192,
+    NID_aria_192, 16, 24, 16, 128,
     0, aria_init_key, NULL,
     EVP_CIPHER_set_asn1_iv,
     EVP_CIPHER_get_asn1_iv,
     NULL)
 IMPLEMENT_BLOCK_CIPHER(aria_256, ks, Aria, EVP_ARIA_KEY,
-    NID_aria_128, 16, 32, 16, 256,
+    NID_aria_256, 16, 32, 16, 128,
     0, aria_init_key, NULL,
     EVP_CIPHER_set_asn1_iv,
     EVP_CIPHER_get_asn1_iv,
@@ -49,7 +45,7 @@ aria_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
 {
 	int ret;
 
-	ret = Aria_set_key(key, ctx->key_len * 8, ctx->cipher_data);
+	ret = ARIA_set_encrypt_key(key, ctx->key_len * 8, ctx->cipher_data);
 
 	if (ret < 0) {
 		EVPerror(EVP_R_ARIA_KEY_SETUP_FAILED);
--- sources/libressl-2.9.0a/include/openssl/aria.h
+++ sources/libressl-2.9.0a/include/openssl/aria.h
@@ -17,13 +17,13 @@ struct aria_key_st {
 };
 typedef struct aria_key_st ARIA_KEY;
 
-void ARIA_encrypt(const unsigned char *in, unsigned char *out,
+void Aria_encrypt(const unsigned char *in, unsigned char *out,
                   const ARIA_KEY *key);
-void ARIA_decrypt(const unsigned char *in, unsigned char *out,
+void Aria_decrypt(const unsigned char *in, unsigned char *out,
                   const ARIA_KEY *key);
-int ARIA_set_encrypt_key(const unsigned char *userKey, const int bits,
+int Aria_set_encrypt_key(const unsigned char *userKey, const int bits,
                          ARIA_KEY *key);
-int ARIA_set_decrypt_key(const unsigned char *userKey, const int bits,
+int Aria_set_decrypt_key(const unsigned char *userKey, const int bits,
                          ARIA_KEY *key);
 
 void Aria_ecb_encrypt(const unsigned char *in, unsigned char *out,
