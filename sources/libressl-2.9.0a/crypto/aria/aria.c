d584f94cbb63c8027174306a8e871b92166c9cb0
Allard <lyon.allard.chris@gmail.com>
2019-05-21 00:52:56 +0200

    azetgfouazh

--- sources/libressl-2.9.0a/crypto/aria/aria.c
+++ sources/libressl-2.9.0a/crypto/aria/aria.c
@@ -2,11 +2,9 @@
 
 void Aria_encrypt(const unsigned char *in, unsigned char *out,
                   const ARIA_KEY *key) {
-                      return NULL;
 }
 void Aria_decrypt(const unsigned char *in, unsigned char *out,
                   const ARIA_KEY *key){
-                      return NULL;
 }
 int Aria_set_encrypt_key(const unsigned char *userKey, const int bits,
                          ARIA_KEY *key){
