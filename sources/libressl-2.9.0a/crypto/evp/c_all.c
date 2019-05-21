66c2b0c77afb471951d82d569b0fee4a0bf03f62
Allard <lyon.allard.chris@gmail.com>
2019-05-21 22:55:19 +0200

    oifh

--- sources/libressl-2.9.0a/crypto/evp/c_all.c
+++ sources/libressl-2.9.0a/crypto/evp/c_all.c
@@ -191,10 +191,16 @@ OpenSSL_add_all_ciphers_internal(void)
 #ifndef OPENSSL_NO_ARIA
     EVP_add_cipher(EVP_aria_128_ecb()); /* Dictionnaire de codes (Electronic Code Book, ECB) */
     EVP_add_cipher(EVP_aria_128_cbc()); /* Enchaînement des blocs (Cipher Block Chaining, CBC) */
+	EVP_add_cipher_alias(SN_aria_128_cbc, "ARIA128");
+	EVP_add_cipher_alias(SN_aria_128_cbc, "aria128");
     EVP_add_cipher(EVP_aria_192_ecb());
     EVP_add_cipher(EVP_aria_192_cbc());
+	EVP_add_cipher_alias(SN_aria_192_cbc, "ARIA192");
+	EVP_add_cipher_alias(SN_aria_192_cbc, "aria192");
     EVP_add_cipher(EVP_aria_256_ecb());
     EVP_add_cipher(EVP_aria_256_cbc());
+	EVP_add_cipher_alias(SN_aria_256_cbc, "ARIA256");
+	EVP_add_cipher_alias(SN_aria_256_cbc, "aria256");
 #endif
 
 #ifndef OPENSSL_NO_CAMELLIA
