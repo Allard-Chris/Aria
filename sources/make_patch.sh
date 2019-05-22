15d61669105d0446b27228bcfb27534c2bd97bce
Allard Chris <chris.allard@atos.net>
2019-05-22 13:09:56 +0200

    Change patch

--- sources/make_patch.sh
+++ sources/make_patch.sh
@@ -11,5 +11,6 @@ echo -e "Generate patch file"
 
 for file in $(find $source/* -type f); do
 	tmp=${file#./aria}
-	diff -Nu $dest$tmp $source$tmp >> $output
+	echo "+++ $dest$tmp" >>$output
+	diff -Nuar $dest$tmp $source$tmp >> $output
 done
