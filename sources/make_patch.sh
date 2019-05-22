2a7fee2a04578ef854bfb2e00e5ee266f53bcf9a
Allard Chris <chris.allard@atos.net>
2019-05-22 13:47:07 +0200

    Final version for make_patch

--- sources/make_patch.sh
+++ sources/make_patch.sh
@@ -3,22 +3,33 @@
 # Run security audit tests
 ####################################################
 
+# Output name for patch file
 output="aria.patch"
+
+# Buffer file for process.
 tmp="buf.tmp"
+
+# Source of our new/modified files
 source="./aria"
+
+# Destination to copy
 dest="./libressl-2.9.0"
 
 echo -e "Generate patch file"
+
+rm $output 2> /dev/null
 
 for file in $(find $source/* -type f); do
 	echo -e $file
 
 	#Get diff between destination and source
-	diff -Naur $dest$tmp $source$tmp > $tmp
+	diff -Naur $dest${file#./aria} $source${file#./aria} > $tmp
 	
 	#Set target to destination file
 	echo "+++ $dest${file#./aria}" >>$output
 
 	#Remove two first lines
-	tail -n +2 $tmp >> $output
+	tail -n +3 $tmp >> $output
 done
+
+rm $tmp 2> /dev/null
