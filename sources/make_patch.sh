3d8f6bd03611fb2e49de4a524e0d8ea17084bdff
Allard Chris <chris.allard@atos.net>
2019-05-22 13:23:25 +0200

    Change make_patch script

--- sources/make_patch.sh
+++ sources/make_patch.sh
@@ -4,13 +4,21 @@
 ####################################################
 
 output="aria.patch"
+tmp="buf.tmp"
 source="./aria"
 dest="./libressl-2.9.0"
 
 echo -e "Generate patch file"
 
 for file in $(find $source/* -type f); do
-	tmp=${file#./aria}
-	echo "+++ $dest$tmp" >>$output
-	diff -Nuar $dest$tmp $source$tmp >> $output
+	echo -e $file
+
+	#Get diff between destination and source
+	diff -Naur $dest$tmp $source$tmp > $tmp
+	
+	#Set target to destination file
+	echo "+++ $dest${file#./aria}" >>$output
+
+	#Remove two first lines
+	tail -n +2 $tmp >> $output
 done
