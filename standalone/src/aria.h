5ae521c7631976b14b2ca1c371139df2807b8b07
Allard <lyon.allard.chris@gmail.com>
2019-05-28 22:10:26 +0200

    Final rendu

--- standalone/src/aria.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_ARIA_H
-#define HEADER_ARIA_H
-
-#define ARIA_ENCRYPT 1
-#define ARIA_DECRYPT 0
-
-int Aria_set_key(const unsigned char *in, const int size, ARIA_KEY *key);
-
-#endif /* HEADER_ARIA_H */
